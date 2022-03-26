9a8475cc91af807e0ee5b71d303630090c6d7e97
cesarhc96 <cesarhcmartinez@gmail.com>
2022-03-26 11:10:35 -0700

    estilos y click

--- app.js
+++ app.js
@@ -125,7 +125,19 @@ window.addEventListener('load', (event) => {
     ];
 
     var canvas = document.getElementById("cvs");
+    var rect = canvas.getBoundingClientRect();
+    // Give the canvas pixel dimensions of their CSS
+    // size * the device pixel ratio.
+    var dpr = 1;
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
     var ctx = canvas.getContext("2d");
+    ctx.scale(dpr, dpr);
+    canvas.onclick = function (event) {
+        if (event.region) {
+            alert('You clicked ' + event.region);
+        }
+    }
 
     var startDate = new Date('2022/04/01 12:00:00');
     var endDate = new Date('2022/04/02 02:00');
@@ -139,7 +151,7 @@ window.addEventListener('load', (event) => {
 
     var stageX = 10;
     var stageY = 60;
-    var stageHeight = 80;
+    var stageHeight = 75;
     var stageWidth = 120;
 
 
@@ -156,14 +168,23 @@ window.addEventListener('load', (event) => {
     while (availableEvent) {
         //console.log(eventHours.getHours());
 
+        // ctx.beginPath();
+        // ctx.fillStyle = 'lightgreen';
+        // ctx.rect(hourX + (hourCounter * hourWidth), hourY, hourWidth, hourHeight);
+        // ctx.fill();
+        // ctx.stroke();
+
+
+        ctx.strokeStyle = "#cb9d41";
         ctx.beginPath();
-        ctx.fillStyle = 'lightgreen';
-        ctx.rect(hourX + (hourCounter * hourWidth), hourY, hourWidth, hourHeight);
-        ctx.fill();
+        ctx.moveTo(hourX + (hourCounter * hourWidth), hourY);
+        ctx.lineTo(hourX + (hourCounter * hourWidth), 1000); // find height
         ctx.stroke();
 
-        ctx.fillStyle = 'black';
-        ctx.fillText(getPrettyHour(eventHours), hourX + (hourCounter * hourWidth), hourY + (hourHeight / 2) + (fontSize / 2));
+
+        ctx.font = 'bolder ' + 16 + 'px Arial';
+        ctx.fillStyle = '7d6128';
+        ctx.fillText(getPrettyHour(eventHours), hourX + (hourCounter * hourWidth) + 5, hourY + (hourHeight / 2) + (fontSize / 2));
         ctx.stroke();
 
 
@@ -175,66 +196,74 @@ window.addEventListener('load', (event) => {
 
     }
 
-
+    ctx.font = '' + 12 + 'px Arial';
+    ctx.font = fontSize + 'px Arial';
     for (var i = 0; i < stages.length; i++) {
-        ctx.beginPath();
-        ctx.fillStyle = 'white';
-        ctx.rect(stageX, stageY + (stageHeight * i), stageWidth, stageHeight);
-        ctx.fill();
-        ctx.stroke();
+        // ctx.beginPath();
+        // ctx.fillStyle = 'white';
+        // ctx.rect(stageX, stageY + (stageHeight * i), stageWidth, stageHeight);
+        // ctx.fill();
+        // ctx.stroke();
 
-        ctx.fillStyle = 'black';
-        ctx.fillText(stages[i], stageX + 10, stageY + (i * stageHeight) + (stageHeight / 2) + (fontSize / 2));
-        ctx.stroke();
+        for (let j = 0; j < 10; j++) {
+            ctx.fillStyle = '#d20047';
+            ctx.fillText(stages[i], stageX + (250 * j), stageY + (i * stageHeight) + fontSize);
+            ctx.stroke();
+        }
+
+
     }
-
-
-
+    ctx.font = fontSize + 'px Arial';
+    var eventRectangles = [];
     for (var i = 0; i < fridayEvents.length; i++) {
-        //console.log(fridayEvents[i]);
 
         var eventLength = getDateDiff(fridayEvents[i].end, fridayEvents[i].start);
         //console.log(eventLength);
         ctx.beginPath();
-        ctx.fillStyle = 'lightblue';
-        //console.log("diif");
-        //console.log(fridayEvents[i].start);
-        //console.log(getDateDiff( startDate,fridayEvents[i].start));
+        ctx.fillStyle = '#d20047';
+
+        var eventRectangle = {
+            x: hourX + (getDateDiff(fridayEvents[i].start, startDate) * hourWidth),
+            y: stageY + (stageHeight * fridayEvents[i].stage) + (fontSize * 1.5),
+            width: (hourWidth * eventLength),
+            height: stageHeight - (fontSize * 2.5),
+            stage: fridayEvents[i].stage,
+            artist: fridayEvents[i].artist,
+            time: getPrettyHour(fridayEvents[i].start) + ' - ' + getPrettyHour(fridayEvents[i].end)
+        };
 
         ctx.rect(
-            hourX + (getDateDiff(fridayEvents[i].start, startDate) * hourWidth),
-            stageY + (stageHeight * fridayEvents[i].stage),
-            (hourWidth * eventLength),
-            stageHeight);
+            eventRectangle.x,
+            eventRectangle.y,
+            eventRectangle.width,
+            eventRectangle.height);
+
+        eventRectangles.push(eventRectangle);
 
         ctx.fill();
+        ctx.strokeStyle = "#85002d";
         ctx.stroke();
 
         ctx.font = 'bolder ' + 14 + 'px Arial';
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = 'white';
         ctx.fillText(
             fridayEvents[i].artist,
             hourX + (getDateDiff(fridayEvents[i].start, startDate) * hourWidth) + (fontSize / 2),
-            stageY + (stageHeight * fridayEvents[i].stage) + (fontSize * 2));
+            stageY + (stageHeight * fridayEvents[i].stage) + (fontSize * 3));
 
-        ctx.font = ' ' + 12 + 'px Arial';
+        ctx.font = 'bold ' + 11 + 'px Arial';
         ctx.fillText(
             getPrettyHour(fridayEvents[i].start) + '-' + getPrettyHour(fridayEvents[i].end),
             hourX + (getDateDiff(fridayEvents[i].start, startDate) * hourWidth) + (fontSize / 2),
             stageY + (stageHeight * fridayEvents[i].stage) + (fontSize * 4));
 
 
-        ctx.stroke();
+        //ctx.stroke();
         ctx.font = fontSize + 'px Arial';
     }
 
-
     function getDateDiff(date1, date2) {
         return Math.abs(date1 - date2) / 36e5;
-    }
-
-    function getDateHours(date) {
-        return ((date.getHours() * 60) + date.getMinutes()) / 60;
     }
 
     function getPrettyHour(date) {
@@ -247,6 +276,30 @@ window.addEventListener('load', (event) => {
     function addHours(date, hours) {
         return date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
     }
+
+    function getMousePosition(canvas, event) {
+        let rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        console.log("Coordinate x: " + x, "Coordinate y: " + y);
+
+        eventRectangles.forEach(rectangle => {
+
+            if (rectangle.x < x && rectangle.y < y && rectangle.x + rectangle.width > x && rectangle.y + rectangle.height > y) {
+                console.log(rectangle);
+                $('#eventStage').html(stages[rectangle.stage]);
+                $('#eventArtist').html(rectangle.artist);
+                $('#eventTime').html(rectangle.time);
+                $('#exampleModal').modal('show');
+            }
+        });
+    }
+
+
+
+    canvas.addEventListener("mousedown", function (e) {
+        getMousePosition(canvas, e);
+    });
 
 });
 
